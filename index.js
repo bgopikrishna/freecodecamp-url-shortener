c7002b38a1cd201f3d3b65f5fe4aa69bcdcacb23
Gopi Krishna <gopiwayz@gmail.com>
2019-11-29 12:09:57 +0530

    added mongodb atlas

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-
-const shortUrls = [
-  {
-    original_url: "https://bgopikrishna.me",
-    short_url: 1
-  }
-];
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.urlencoded());
 
 app.use(cors({ optionSuccessStatus: 200 }));
+
+mongoose
+  .connect(
+    "mongodb+srv://mistborn:fccDatabase@personalcluster-gx87z.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch(err => {
+    console.log("Error mongodb", err);
+  });
+
+const shortUrlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+});
+
+let idTracking = 0;
+
+const ShortUrl = mongoose.model("ShortUrls", shortUrlSchema);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -23,12 +37,9 @@ app.post("/api/shorturl/new", (req, res) => {
   const isValidUrl = verifyValidyofUrl(url);
 
   if (isValidUrl) {
-    const newShortendUrl = {
-      original_url: url,
-      short_url: shortUrls.length + 1
-    };
-    shortUrls.push(newShortendUrl);
-    res.send(newShortendUrl);
+    createShortUrl(url).then(newShortendUrl => {
+      res.send(newShortendUrl);
+    });
   } else {
     res.send({
       error: "No short url found for given input"
@@ -38,22 +49,39 @@ app.post("/api/shorturl/new", (req, res) => {
 
 app.get("/api/shorturl/:id", (req, res) => {
   const shortUrlId = parseInt(req.params.id);
-  const urlObj = shortUrls.find(url => parseInt(url.short_url) === shortUrlId);
-  if (urlObj) {
-    res.redirect(urlObj.original_url);
-  } else {
-    res.send({
-      error: "No short url found for given input"
-    });
-  }
-
-  res.end();
+  getUrl(shortUrlId).then(urlObj => {
+    if (urlObj && urlObj.original_url) {
+      res.redirect(urlObj.original_url);
+    } else {
+      res.send({
+        error: "No short url found for given input"
+      });
+    }
+  });
 });
 
 const port = process.env.PORT || 4000;
 var listener = app.listen(port, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+async function createShortUrl(url) {
+  idTracking++;
+  const newUrl = new ShortUrl({
+    original_url: url,
+    short_url: Math.floor(Math.random() * 10)
+  });
+
+  const result = await newUrl.save();
+  return result;
+}
+
+async function getUrl(id) {
+  console.log("getting url", id);
+  const url = await ShortUrl.find({ short_url: id });
+  console.log("got url", url);
+  return url[0];
+}
 
 function verifyValidyofUrl(url) {
   const urlRegex = /(http[s]?:\/\/)?[^\s(["<,>]*\.[^\s[",><]*/gim;
